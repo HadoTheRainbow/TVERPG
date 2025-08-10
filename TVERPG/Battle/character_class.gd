b60d554584ee4816c95d32b979319a57f9d1cc75
HadoTheRainbow <hadopelagicrainbow@gmail.com>
2025-08-10 17:13:31 -0400

    Add files via upload

--- TVERPG/Battle/character_class.gd
+++ TVERPG/Battle/character_class.gd
@@ -1,10 +1,13 @@
-extends Node
+extends CharacterEntity
 
 class_name Character_cust
 
 var skill_name_dict = SkillsDict.new()
 
-var id: int
+
+var connector
+
+var ent_id: int
 var lvl: int
 var char_name: String
 var hp: int
@@ -15,6 +18,21 @@ var speed: int
 var magic: int
 var status_effects: Array = []
 var is_hero: bool
+var armor_pierce: int
+
+var character_downed = false
+var character_disabled = false
+
+var default_values = []
+
+var hit_rate_mod
+var crit_rate_mod
+var crit_damage_mod
+var effect_hit_rate_mod
+var effect_duration_mod
+
+
+
 var resistances = [1,1,1,1,1,1,1,1,1,1,1,1]
 var default_normal_type = 0
 var type_override = -1
@@ -57,13 +75,17 @@ func _init(data: Array):
 		speed = data[4]
 		magic = data[5]
 func reload():
+	skill_name_dict.connector = self
 	char_name = char_name
 	max_hp = apply_level(max_hp, lvl, 1.1)
 	hp = max_hp
 	attack = apply_level(attack, lvl, 1.1)
 	defense = apply_level(defense, lvl, 1.1)
 	speed = speed
-	magic = magic
+	magic = apply_level(magic, lvl, 1.1)
+	
+	default_values = [max_hp, attack, defense, speed, magic]
+	
 	
 func get_stat(a):
 	stats_reference = {
@@ -74,10 +96,11 @@ func get_stat(a):
 	
 	
 	}
+	print(a)
 	return stats_reference[a]
 	
+	
 func get_all_attack_stats():
-	print([char_name, max_hp, attack, defense, speed, magic])
 	return [char_name, max_hp, attack, defense, speed, magic]
 
 func apply_level(value,level,level_scaling) -> int:
@@ -105,10 +128,86 @@ func heal(amount: int):
 		return "MAX"
 	else:
 		return hp - old_hp
+
+func update_attack(value, multiplicative):
+	if multiplicative:
+		attack *= value
+	else:
+		attack += value
+
+func update_defense(value, multiplicative):
+	if multiplicative:
+		defense *= value
+	else:
+		defense += value
+
+func update_speed(value, multiplicative):
+	if multiplicative:
+		speed *= value
+	else:
+		speed += value
+
+func update_magic(value, multiplicative):
+	if multiplicative:
+		magic *= value
+	else:
+		magic += value
+
+func update_hit_rate(value, multiplicative):
+	if multiplicative:
+		hit_rate_mod *= value
+	else:
+		hit_rate_mod += value
+
+func update_crit_rate(value, multiplicative):
+	if multiplicative:
+		crit_rate_mod *= value
+	else:
+		crit_rate_mod += value
+
+func update_crit_damage(value, multiplicative):
+	if multiplicative:
+		crit_damage_mod *= value
+	else:
+		crit_damage_mod += value
+
+func update_effect_hit_rate(value, multiplicative):
+	if multiplicative:
+		effect_hit_rate_mod *= value
+	else:
+		effect_hit_rate_mod += value
+
+func update_effect_duration(value, multiplicative):
+	if multiplicative:
+		hit_rate_mod *= value
+	else:
+		hit_rate_mod += value
+
+
+class Kat:
+	extends Character_cust
 	
 	
-	
-	
+	func _init():
+		id = 0
+		char_name = "Kat"
+		hp = 74
+		max_hp = 74
+		attack = 27
+		defense = 7
+		speed = 16
+		magic = 24
+		status_effects = []
+		resistances[0] = 0.80
+		resistances[4] = 1.20
+		skills = [
+			skill_name_dict.character_list["Normal"], 
+			skill_name_dict.character_list["Overclock"], 
+			skill_name_dict.character_list["Hit"], 
+			skill_name_dict.character_list["DualStrike"], 
+			skill_name_dict.character_list["Blaze"], 
+			
+		]
 	
 class SkeletonGeneric:
 	extends Character_cust
@@ -120,7 +219,7 @@ class SkeletonGeneric:
 		hp = 60
 		max_hp = 60
 		attack = 18
-		defense = 11
+		defense = 6
 		speed = 14
 		magic = 2
 		status_effects = []
@@ -139,14 +238,12 @@ class FireSlime:
 		hp = 45
 		max_hp = 45
 		attack = 12
-		defense = 7
+		defense = 2
 		speed = 16
 		magic = 2
 		status_effects = []
 		resistances[1] = -99.00
 		resistances[2] = 2.00
-		max_hp = max_hp
-		hp = max_hp
 		type_override = 1
 		skills = [
 			skill_name_dict.character_list["Normal"], 
@@ -172,6 +269,13 @@ class StormPrincess:
 		status_effects = []
 		for i in resistances.size():
 			resistances[i] = 0.50
-		max_hp = max_hp
-		hp = max_hp
 		type_override = 12
+		skills = [
+			skill_name_dict.character_list["Normal"], 
+			skill_name_dict.character_list["InfinitePain"], 
+			skill_name_dict.character_list["Blaze"], 
+			skill_name_dict.character_list["Heal"], 
+			skill_name_dict.character_list["DualStrike"], 
+			skill_name_dict.character_list["Fury"], 
+			
+		]
